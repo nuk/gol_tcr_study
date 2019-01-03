67859196941f2098a8b9554adb9964f87d13c99e
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2019-01-03 16:24:00 -0200

    TCR STEP

--- tcr.sh
+++ tcr.sh
@@ -1,5 +1,10 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 TEST="rspec"
 COMMIT="git add . && git commit -am 'TCR STEP'"
-REVERT="git reset --hard HEAD~1"
+REVERT='git reset --hard HEAD~1 && echo -e "${RED} [TCR] REVERTED"'
 
 (eval $COMMIT) && (eval $TEST) || (eval $REVERT)
